76857712938ffa22047c1427934d981ad9bfaae1
Rohith_k <2100032432cse@gmail.com>
2024-08-03 11:34:03 +0530

    frist instanace

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('server');
+const express=require('express')
+const server=express()
+
+server.get('/',(req,res)=>{
+    res.send('frist page')
+})
+server.listen(3006,()=>{
+    console.log('server listing');
+    
+})
